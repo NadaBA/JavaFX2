6719d41a33b4b1cc5b0264ecb1cf5c24980e53ac
nannawagner <kubax@live.dk>
2020-02-19 16:27:37 +0100

    nu med flow pane

--- src/main/java/User_interface.java
+++ src/main/java/User_interface.java
@@ -1,6 +1,8 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -13,7 +15,7 @@ public class User_interface extends Application {
         Label nannasLabel = new Label("Hej fra Nanna");
 
         Label label = new Label("Hello form JavaFX");
-        StackPane stackPane = new StackPane(label);
+        Pane stackPane = new FlowPane(label);
         stackPane.getChildren().add(nannasLabel);
         Scene scene = new Scene(stackPane, 640, 480);
         stage.setScene(scene);
