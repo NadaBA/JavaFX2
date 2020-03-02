adfdc78bbfeeca4c2cd21d9b93d09f1232161a35
Payam <danieljohari2610@gmail.com>
2020-03-02 10:03:03 +0100

    Baggrundkode på knapper, samt generel baggrundskode.

--- src/main/java/User_interface.java
+++ src/main/java/User_interface.java
@@ -1,15 +1,19 @@
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.TilePane;
+import javafx.scene.layout.*;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
 import java.io.FileInputStream;
+import java.util.Scanner;
 
 
 public class User_interface extends Application {
@@ -26,51 +30,133 @@ public class User_interface extends Application {
             //billede
 
             //Ved MAC brug nedenstående syntaks:
-            //FileInputStream input = new FileInputStream("/Users/Nada/Desktop/Sejged2.jpg"); //MAC OS
+            //FileInputStream inputOn = new FileInputStream("/Users/Nada/Desktop/Sejged2.jpg"); //MAC OS
 
-            FileInputStream input = new FileInputStream("C:\\Users\\Payam\\Desktop\\ON.png");
+            FileInputStream inputOn = new FileInputStream("C:\\Users\\Payam\\Desktop\\UON.jpg");
+            FileInputStream inputOff =  new FileInputStream("C:\\Users\\Payam\\Desktop\\UOFF.jpg");
 
-            Image image = new Image(input);
+            Image imageON = new Image(inputOn);
+            Image imageOFF = new Image(inputOff);
 
 
             //billede view
-            ImageView iw = new ImageView(image);
-            ImageView ie = new ImageView(image);
+            ImageView iw = new ImageView(imageON);
+            ImageView ie = new ImageView(imageOFF);
 
-            Button bOn = new Button("On", iw);
-            Button bOff = new Button("Off", ie);
-            Button bUpdateTemp = new Button("Update Temp");
+            //KNAPPER
+            Button bOn = new Button("", iw);
+            Button bOff = new Button("", ie);
+            Button bUpdateTemp = new Button("Opdater Temp");
+            Button bUpdateBpm = new Button("Opdater BPM");
 
-            bOn.setStyle("-fx-background-color: green");
-            bOff.setStyle("-fx-background-color: red");
+            bOn.setStyle("-fx-background-color: white");
+            bOff.setStyle("-fx-background-color: white");
+
+            //FORSKELLIGE PANES OPRETTELSE
+            BorderPane borderPane = new BorderPane();
+            AnchorPane anchorPane = new AnchorPane();
+            GridPane gridPaneCenter = new GridPane();
+            FlowPane flowPane = new FlowPane();
+            GridPane gridPaneRight = new GridPane();
+            GridPane gridPaneLeft = new GridPane();
 
 
-            TilePane tilePane = new TilePane();
 
 
+            //LABELS
             Label systemOfflineLabel = new Label("Patient Monitoring System Offline");
             Label labelKlik = new Label("Klik på 'On' eller 'Off, for at tænde eller slukke");
-            Label labelIntet = new Label("IT Projekt 1, 2. Semester");
-            Label tempLabel = new Label("Patientens Temperatur: " + getValue());
+            Label tempLabel = new Label("Patientens Temperatur: " + getTemp());
+            Label bpmLabel = new Label("Patientens BPM: " + getBPM());
             TextField textField = new TextField("Skriv en kommentar:");
-            tilePane.getChildren().add(0, bOn);
-            tilePane.getChildren().add(1, bOff);
-            tilePane.getChildren().add(2, systemOfflineLabel);
-            tilePane.getChildren().add(3, labelKlik);
-            tilePane.getChildren().add(4,labelIntet);
-            tilePane.getChildren().add(5, textField);
-            tilePane.getChildren().add(6, tempLabel);
-            tilePane.getChildren().add(7, bUpdateTemp);
-            tilePane.setStyle("-fx-background-color: lightblue");
+            textField.setPrefSize(300,400);
+
+            //ANCHORPANE
+/*
+            HBox hb = new HBox();
+            anchorPane.getChildren().addAll(bOn);
+            anchorPane.getChildren().addAll(bOff);
+            anchorPane.getChildren().addAll(hb);
+            anchorPane.setMinSize(300,100);
+            AnchorPane.setBottomAnchor(bOff,10.0);
+            AnchorPane.setTopAnchor(bOn,10.0);
+            anchorPane.setStyle("-fx-background-color: white");
+
+ */
+            //GRIDPANE VENSTRE
+            gridPaneLeft.setPadding(new Insets(10,10,10,10));
+            gridPaneLeft.setMinSize(300,300);
+            gridPaneLeft.setVgap(5);
+            gridPaneLeft.setHgap(5);
+
+            gridPaneLeft.add(bOn,0,20);
+            gridPaneLeft.add(bOff,0,21);
+            gridPaneLeft.setStyle("-fx-background-color: white");
 
 
 
 
 
+            //GRIDPANE HØJRE
+            gridPaneRight.setPadding(new Insets(10,10,10,10));
+            gridPaneRight.setMinSize(300,300);
+            gridPaneRight.setVgap(5);
+            gridPaneRight.setHgap(5);
+
+            Text Navn = new Text("Navn:");
+            gridPaneRight.add(Navn,0,0);
+
+            TextField textNavn = new TextField();
+            textNavn.setPrefColumnCount(10);
+            gridPaneRight.add(textNavn,1,0);
+
+            Text efterNavn = new Text("Efternavn:");
+            gridPaneRight.add(efterNavn,0,1);
+
+            TextField textEfterNavn = new TextField();
+            textEfterNavn.setPrefColumnCount(10);
+            gridPaneRight.add(textEfterNavn,1,1);
+
+            Text Alder = new Text("Alder:");
+            gridPaneRight.add(Alder,0,2);
+
+            TextField textAlder = new TextField();
+            textAlder.setPrefColumnCount(10);
+            gridPaneRight.add(textAlder,1,2);
+
+            gridPaneRight.add(textField,1,8);
+
+            gridPaneRight.setStyle("-fx-background-color: lightblue");
 
 
-            //aktions event
-            Scene scene = new Scene(tilePane);
+
+
+            //GRIDPANE CENTER
+            gridPaneCenter.setPadding(new Insets(10,10,10,10));
+            gridPaneCenter.setMinSize(300,300);
+            gridPaneCenter.setVgap(5);
+            gridPaneCenter.setHgap(5);
+            gridPaneCenter.add(systemOfflineLabel,0,0);
+            gridPaneCenter.add(labelKlik,0,1);
+            gridPaneCenter.add(tempLabel,0,10);
+            gridPaneCenter.add(bUpdateTemp,3,10);
+            gridPaneCenter.add(bpmLabel,0,20);
+            gridPaneCenter.add(bUpdateBpm,3,20);
+            gridPaneCenter.setStyle("-fx-background-color: white");
+
+
+
+
+            //BORDERPANE
+            borderPane.setLeft(gridPaneLeft);
+            borderPane.setRight(gridPaneRight);
+            borderPane.setCenter(gridPaneCenter);
+
+
+
+            //Aktions event af scene og knapper under
+            Scene sceneBorderPane = new Scene(borderPane);
+
 
 
             EventHandler<ActionEvent> event = new EventHandler<ActionEvent>() {
@@ -86,34 +172,51 @@ public class User_interface extends Application {
             EventHandler<ActionEvent> event1 = new EventHandler<ActionEvent>() {
                 @Override
                 public void handle(ActionEvent event) {
-                    systemOfflineLabel.setText("Patient Monitoring System.........Offline....");
+                    systemOfflineLabel.setText("Patient Monitoring System Offline");
                 }
             };
             bOff.setOnAction(event1);
 
-
+                //EVENT AF TEMP MED KLIK PÅ KNAP
                 EventHandler<ActionEvent> eventTemp = new EventHandler<ActionEvent>() {
                     @Override
                     public void handle(ActionEvent event) {
-                        tempLabel.setText("Patientens Temperatur: " + getValue());
-                        if (getValue() >= 39){
-                            Label warningLabel = new Label("Patientens temperatur er over 39!!");
-                            System.out.println("gør noget!");
-                            tilePane.getChildren().add(8,warningLabel);
-                            warningLabel.setStyle("-fx-text-fill: red");
+                        tempLabel.setText("Patientens Temperatur: " + getTemp());
+                        if (getTemp() >= 39){
+                            Label warningTempLabel = new Label("Patientens temperatur er over 39!!");
+                            System.out.println("ALARM TEMP!");
+                            gridPaneCenter.add(warningTempLabel,0,15);
+                            warningTempLabel.setStyle("-fx-text-fill: red");
                         }
                     }
                 };
                 bUpdateTemp.setOnAction(eventTemp);
 
-                primaryStage.setScene(scene);
+                //EVENT AF BPM MED KLIK AF KNAP
+                EventHandler<ActionEvent> eventBPM = new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent event) {
+                        bpmLabel.setText("Patientens BPM: " + getBPM());
+                        if (getBPM() >= 120){
+                            Label warningBpmLabel = new Label("Patientens BPM er over 120!");
+                            System.out.println("ALARM BPM");
+                            gridPaneCenter.add(warningBpmLabel,0,25);
+                            warningBpmLabel.setStyle("-fx-text-fill: red");
+
+                        }
+                    }
+                };
+                bUpdateBpm.setOnAction(eventBPM);
+
+
+                primaryStage.setScene(sceneBorderPane);
                 //primaryStage.setScene(scene1);
                 primaryStage.show();
             }
 
 
 
-        double getValue () {
+        double getTemp() {
 
             while(true) {
                 double value = Math.random() * 60 + 150;
@@ -127,7 +230,21 @@ public class User_interface extends Application {
             }
     }
 
+    double getBPM(){
 
+            while(true) {
+                double alder = 25;
+                double maxBPM = 220 - alder;
+                double targetHrRandom = Math.random() * maxBPM;
+                double targetHrMin = 0.45 * maxBPM;
+                //System.out.println(targetHrMin);
+                double targetHR = (targetHrRandom + targetHrMin) / 2;
+                targetHR = Math.round(targetHR);
+                //System.out.println(targetHR);
+
+                return targetHR;
+            }
+    }
 
 
 
